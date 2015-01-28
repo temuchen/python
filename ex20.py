f3e09d5bfa8e2aa564d4287d16d332bb95024e25
temuchen <controlsys@163.com>
2015-01-28 11:50:18 +0800

    20150128

--- ex20.py
+++ ex20.py
@@ -26,9 +26,9 @@ print "let's print three lines:"
 current_line=1
 print_a_line(current_line,current_file)
 
-current_line=current_line+1
+current_line+=1
 print_a_line(current_line,current_file)
 
-current_line=current_line+1
+current_line+=1
 print_a_line(current_line,current_file)	
 			
